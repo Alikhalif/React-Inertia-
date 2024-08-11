5935b55f9238114e7a4d4efb75120c577b81a2e2
Alikhalif <abdelalikhalif1999@gmail.com>
2024-08-11 23:36:58 +0100

    most replayed

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,7 +5,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 
-export default function Authenticated({ auth, header, children }) {
+export default function AuthenticatedLayout({ auth, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
@@ -21,8 +21,25 @@ export default function Authenticated({ auth, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                                <NavLink
+                                    href={route('dashboard')}
+                                    active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                                <NavLink
+                                    href={route('project.index')}
+                                    active={route().current('project.index')}>
+                                    Projects
+                                </NavLink>
+                                <NavLink
+                                    href={route('task.index')}
+                                    active={route().current('task.index')}>
+                                    All Tasks
+                                </NavLink>
+                                <NavLink
+                                    href={route('user.index')}
+                                    active={route().current('user.index')}>
+                                    Users
                                 </NavLink>
                             </div>
                         </div>
@@ -76,7 +93,7 @@ export default function Authenticated({ auth, header, children }) {
                                         strokeLinejoin="round"
                                         strokeWidth="2"
                                         d="M4 6h16M4 12h16M4 18h16"
-                                        
+
                                     />
                                     <path
                                         className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
